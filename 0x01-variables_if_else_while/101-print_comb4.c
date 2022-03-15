df3ee3e1d0dacd07ee6d43ae3f75070b410eb486
emmanuel <mcdecoil@gmail.com>
2022-03-15 01:26:02 +0000

    ok

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 48; n < 59; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 49; m < 59; m++)
+		for (m = 49; m < 58; m++)
 		{
-			for (l = 50; l < 59; l++)
+			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
@@ -30,5 +30,6 @@ int main(void)
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
