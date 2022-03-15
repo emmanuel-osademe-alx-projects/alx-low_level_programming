6161528789f908b0edb338619af2d3ea44d4facf
emmanuel <mcdecoil@gmail.com>
2022-03-15 00:37:35 +0000

    ok

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 # 0x01. C - Variables, if, else, while
 - 0-positive or negative.c checks random value for conditions
 - 1-last digit.c getting last digit
+- 2-print alphabet.c
