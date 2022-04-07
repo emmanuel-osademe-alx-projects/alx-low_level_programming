fafa88ce8e410a72fa25a8b39a579cc8afe9daba
emmanuel <mcdecoil@gmail.com>
2022-04-07 13:59:36 +0000

    ok

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - returns half of a string
+ * @str: takes a pointer address
+ *
+ * Return: void.
+ */
+
+void puts_half(char *str)
+{
+	int i, len = 0, mid;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (length % 2 == 0)
+	{
+		mid = len / 2;
+
+		for (i = mid; i < len; i++)
+			_putchar(str[i]);
+	}
+
+	else
+	{
+		mid = (len - 1) / 2;
+
+		for (i = mid + 1; i < len; i++)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
+
