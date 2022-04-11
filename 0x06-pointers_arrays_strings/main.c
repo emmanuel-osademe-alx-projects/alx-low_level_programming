e950c55ff367b5a339e9c8b6d7104663cc4e4b76
emmanuel <mcdecoil@gmail.com>
2022-04-11 06:43:34 +0000

    ok

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
+    char s1[98] = "Fantastic Beasts ";
+    char s2[] = "& The Early life of Dumbledore!\n";
     char *ptr;
 
     printf("%s\n", s1);
