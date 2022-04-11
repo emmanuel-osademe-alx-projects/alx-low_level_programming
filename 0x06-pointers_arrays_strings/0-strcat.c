d49d9eb280f212a6603b68f16476daf4d2a3a1c0
emmanuel <mcdecoil@gmail.com>
2022-04-11 07:10:54 +0000

    lol

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,24 +10,29 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len, len2, len3, i;
+while (*dest != 0)	/* while dest is not null-terminating char */
+dest++;		/* get to the end */
 
-	len = 0, len2 = 0;
+while (*src != 0)	/* while src is not the null-terminating char */
+{
+src++;
+*dest = *src;
+}
 
-	while (dest[len] != 0)
-		len++;
-
-	while (src[len2] != 0)
-		len2++;
-
-	for (i = 0; i <= len2; i++)
-	{
-		dest[len + i] = src[i];
-	}
-
-	len3 = len + len2 + 1;
-
-	dest[len3] = '\n';
-
-	return (dest);
+return (dest);
+	/**
+	*int len, len2, len3, i;
+	*len = 0, len2 = 0;
+	*while (dest[len] != 0)
+	*	len++;
+	*while (src[len2] != 0)
+	*	len2++;
+	*for (i = 0; i <= len2; i++)
+	*{
+	*	dest[len + i] = src[i];
+	*}
+	*len3 = len + len2 + 1;
+	*dest[len3] = '\n';
+	*return (dest);
+	*/
 }
