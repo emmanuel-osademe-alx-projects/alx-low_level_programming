a9043373670514d5f93df6c0e67aad26430b4d18
emmanuel <mcdecoil@gmail.com>
2022-05-04 12:00:40 +0000

    Read file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - prints to sdout the number of letters provided
+ * @filename: filename
+ * @letters: number of letters to print
+ * Return: int number of characters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd, wr;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+
+	if (!buffer)
+		return (0);
+
+	rd = read(fd, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+
+	close(fd);
+
+	free(buffer);
+
+	return (wr);
+}
