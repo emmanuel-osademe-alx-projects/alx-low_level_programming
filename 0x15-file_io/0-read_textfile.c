72354d81ffb45637a2c82cec5aebfab4f937e7ce
emmanuel <mcdecoil@gmail.com>
2022-05-04 13:14:13 +0000

    fail betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,10 +7,10 @@
  * read_textfile - prints to sdout the number of letters provided
  * @filename: filename
  * @letters: number of letters to print
- * Return: int number of characters printed
+ * Return: ssize_t number of characters printed
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char* filename, size_t letters)
 {
 	int fd;
 	ssize_t rd, wr;
@@ -31,6 +31,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	rd = read(fd, buffer, letters);
+
+	if (rd == -1)
+		return (0);
+
 	wr = write(STDOUT_FILENO, buffer, rd);
 
 	if (wr == -1)
