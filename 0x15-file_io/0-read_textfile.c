fb3c7866764dd28a821179d6e694819fc23d64f4
emmanuel <mcdecoil@gmail.com>
2022-05-04 13:31:08 +0000

    compress all

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,40 +3,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-
+/**
+ * read_textfile - prints to sdout the number of letters provided
+ * @filename: filename
+ * @letters: number of letters to print
+ * Return: ssize_t number of characters printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t rd, wr;
 	char *buffer;
 
-	/* check if file is null */
 	if (!filename)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		return (0);
-
 	buffer = malloc(sizeof(char) * (letters));
-
-	if (!buffer)
+	if (fd == -1 || !buffer)
 		return (0);
-
 	rd = read(fd, buffer, letters);
-
 	if (rd == -1)
 		return (0);
-
 	wr = write(STDOUT_FILENO, buffer, rd);
-
-	if (wr == -1)
-		return (0);
-
 	close(fd);
-
 	free(buffer);
-
 	return (wr);
 }
