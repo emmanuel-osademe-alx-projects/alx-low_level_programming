83fb90b1bc23dbb114ae8dc7fd830096e23483f6
emmanuel <mcdecoil@gmail.com>
2022-05-04 12:57:47 +0000

    error fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	rd = read(fd, buffer, letters);
 	wr = write(STDOUT_FILENO, buffer, rd);
 
-	if (wr !== letters)
+	if (wr != letters)
 		return (0);
 
 	close(fd);
